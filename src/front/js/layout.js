aeb34e887f8414460476fc7eea03187b5030b39d
Keialex17 <115053607+Keialex17@users.noreply.github.com>
2023-07-13 16:58:57 +0000

    Ahi va

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,9 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Login } from "./pages/login.js";
+import { Signup } from "./pages/signup.js";
+import { Private } from "./pages/private.js";
 
 //create your first component
 const Layout = () => {
@@ -29,6 +32,9 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
+                        <Route element={<Signup />} path="/signup" />
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<Private />} path="/private" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
